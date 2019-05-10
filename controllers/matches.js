79170376010cad4127c1596fcffec3981a7ea8b4
Kristian Stafford Smith <kristianss7890@gmail.com>
2019-05-10 16:31:34 +0100

    Added interests to matching algorithm, removed JSON.stringify()

--- controllers/matches.js
+++ controllers/matches.js
@@ -1,15 +1,13 @@
 const User = require('../models/User')
 
 
-
-
-
 function matchRoute(req, res, next) {
   //get all the users from the database
   User.find()
     .then((users) => {
       //change the ID from the url to a string
-      const userId = JSON.stringify(req.params.id)
+      //const userId = JSON.stringify(req.params.id)
+      const userId = req.params.id
       //initialize variables to store the users matching details
       let userLocation = ''
       let userGender = ''
@@ -17,49 +15,45 @@ function matchRoute(req, res, next) {
 
       //find the location of the user searching for a match
       users.forEach((user) => {
-        const userDbId = JSON.stringify(user._id)
 
-        if(userDbId === userId) {
-          userLocation = JSON.stringify(user.location)
-          userGender = JSON.stringify(user.gender)
+        if(user._id.equals(req.params.id)) {
+          userLocation = user.location
+          userGender = user.gender
           //create an array of user interests
-          userInterests = JSON.stringify(user.interests).split(', ' || ' ' || ',')
+          userInterests = user.interests.split(', ')
         }
 
       })
-
-      console.log(userLocation)
-      console.log(userInterests)
-
-
-
-
 
       //initialize an array in which to store your matches
       const arrayOfMatches = []
 
       //loop through all users to find which ones match, and exclude yourself
       users.forEach((match) => {
-        const matchLocation = JSON.stringify(match.location)
-        const matchGender = JSON.stringify(match.gender)
-        const matchId = JSON.stringify(match._id)
-        const matchInterests = JSON.stringify(match.interests).split(', ')
+        const matchLocation = match.location
+        const matchGender = match.gender
+        const matchId = match._id
+        const matchInterests = match.interests.split(', ')
+        const similarInterests = []
+
+        //count the number of similar interests
+        matchInterests.forEach((interest) => {
+          if(userInterests.includes(interest)) {
+            similarInterests.push(interest)
+          }
+        })
+
         //if IDs dont match and the locations match, push into array
-        if(userId !== matchId && userLocation === matchLocation && userGender !== matchGender) {
+        if(userId !== matchId && userLocation === matchLocation && userGender !== matchGender && similarInterests.length > 2) {
           arrayOfMatches.push(match)
         }
 
-        console.log(match.username, matchInterests)
       })
       //return the arrayOfMatches as JSON
       return res.json(arrayOfMatches)
-
     })
     .catch(next) //catch any errors
-
 }
-
-
 
 
 
