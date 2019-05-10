fa46dbc01d8608ac9c14ebbfe9996261b57a7efa
Elle Whaley <elliewhaley11@gmail.com>
2019-05-10 14:23:00 +0100

    register pushes to login, login pushes to home

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { HashRouter as Router, Route, Switch} from 'react-router-dom'
 import 'bulma'
 
-
 import Home from './components/Home'
 import Register from './auth/Register'
 import Login from './auth/Login'
