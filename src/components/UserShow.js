359233528019a01de5fbe0009db614b952c1a36c
Elle Whaley <elliewhaley11@gmail.com>
2019-05-12 17:56:31 +0100

    added an edit button to profiles added only by that user

--- src/components/UserShow.js
+++ src/components/UserShow.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
+import Auth from '../lib/Auth'
 
 class UserShow extends React.Component {
 
@@ -14,6 +16,10 @@ class UserShow extends React.Component {
   componentDidMount() {
     axios.get(`api/users/${this.props.match.params.id}`)
       .then(res => this.setState({ user: res.data }))
+  }
+
+  canModify() {
+    return Auth.isAuthenticated() && Auth.getPayload().sub === this.state.user._id
   }
 
   render() {
@@ -41,6 +47,11 @@ class UserShow extends React.Component {
               <p className="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
           </div>
+          {this.canModify() &&
+            <div className="level-right">
+              <Link to={`/users/${this.state.user._id}/edit`} className="button is-primary">Edit</Link>
+            </div>
+          }
         </div>
       </section>
     )
