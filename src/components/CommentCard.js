0ec2399b67ce7283d325fd0fe264f7859d80470c
Elle Whaley <elliewhaley11@gmail.com>
2019-05-15 17:29:32 +0100

    fixed buttons that were skewed in merge

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -7,30 +7,17 @@ const CommentCard = ({ content, user }) => {
       <article className="media">
 
 
-        <figure classNameName="media-left">
+        <figure className="media-left">
           <img className="image-comment" src={user.image} alt={user.username}/>
         </figure>
-        <div classNameName="media-content">
-          <div classNameName="content">
+        <div className="media-content">
+          <div className="content">
             <p>
               <strong>{user.username}</strong> <small>{content.createdAt}</small>
               <br/>
               {content}
             </p>
           </div>
-          <nav classNameName="level is-mobile">
-            <div className="level-left">
-              <a className="level-item">
-                <span className="icon is-small"><i className="fas fa-reply"></i></span>
-              </a>
-              <a className="level-item">
-                <span className="icon is-small"><i className="fas fa-retweet"></i></span>
-              </a>
-              <a className="level-item">
-                <span className="icon is-small"><i className="fas fa-heart"></i></span>
-              </a>
-            </div>
-          </nav>
         </div>
         <div className="media-right">
           <button className="delete"></button>
