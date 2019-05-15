2e44c3fd5721d725e2af8cae2b1736303cd5176e
Paul Aliu <paul.iyola.aliu@gmail.com>
2019-05-15 14:23:35 +0100

    created an array in images.js

---
+++ src/images.js
@@ -0,0 +1,14 @@
+const backgrounds = [
+
+  './images/background-images/coupleclouds.jpg',
+  './images/background-images/blacksands.jpg',
+  './images/background-images/couple-sunset.jpg',
+  './images/background-images/couple.jpg',
+  './images/background-images/crossroads.jpg',
+  './images/background-images/family-fun.jpg',
+  './images/background-images/london.jpg',
+  './images/background-images/social.jpg',
+  './images/background-images/woman-coffee.jpg'
+]
+
+export default backgrounds
