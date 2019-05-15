b59c2969ecdfb47f5a8ca9692def2fa7eaa967de
Detola <oyawoye.ade@gmail.com>
2019-05-15 23:28:41 +0100

    Added error messages to Register.js when field is empty retun a message

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 
+import Footer from '../components/Footer'
+
 class Register extends React.Component {
   constructor() {
     super()
@@ -46,76 +48,99 @@ class Register extends React.Component {
 
   // within render, added a dropdown menu for location.
   render() {
-    console.log(this.state.errors.username)
-    console.log(this.state.errors.email)
+    console.log(this.state, 'username')
+    console.log(this.state.errors.email, 'email')
     return (
-      <section className="section">
-        <div className="container">
-          <div className="columns is-centered">
-            <div className="column is-half-desktop is-two-thirds-tablet">
-              <form onSubmit={this.handleSubmit}>
+      <section>
+        <section className="section">
+          <div className="container">
+            <div className="columns is-centered">
+              <div className="column is-half-desktop is-two-thirds-tablet">
+                <form onSubmit={this.handleSubmit}>
 
-                <div className="field">
-                  <label className="label">Username</label>
-                  <div className="control">
-                    <input className="input" name="username" placeholder="eg: leela3000" onChange={this.handleChange} />
-                  </div>
-                </div>
-
-                <div className="field">
-                  <label className="label">Email</label>
-                  <div className="control">
-                    <input className="input" name="email" placeholder="eg: leela@planetexpress.nnyc" onChange={this.handleChange} />
-                  </div>
-                </div>
-
-                <div className="register-container">
                   <div className="field">
-                    <label className="label">Location</label>
-                    <div className="select">
-                      <select name="location" onChange={this.handleChange}>
-                        <option value="">Select</option>
-                        <option value="Amsterdam">Amsterdam</option>
-                        <option value="london">London</option>
-                        <option value="manchester">Manchester</option>
-                        <option value="birmingham">Birmingham</option>
-                        <option value="mexico-city">Mexico City</option>
-                        <option value="Berlin">Berlin</option>
-                        <option value="Paris">Paris</option>
-                        <option value="Brussels">Brussels</option>
-                        <option value="Qubec">Qubec</option>
-                        <option value="Montreal">Montreal</option>
-                        <option value="New York">New York</option>
-                        <option value="Venice">Venice</option>
-                      </select>
+                    <label className="label">Username</label>
+                    <div className="control">
+                      <input className="input"
+                        name="username"
+                        placeholder="eg: leela3000"
+                        onChange={this.handleChange} />
+                    </div>
+                    {this.state.errors.username && <div className="help is-danger">{this.state.errors.username}</div>}
+                  </div>
+
+                  <div className="field">
+                    <label className="label">Email</label>
+                    <div className="control">
+                      <input className="input"
+                        name="email"
+                        placeholder="eg: leela@planetexpress.nnyc" onChange={this.handleChange} />
+                    </div>
+                    {this.state.errors.email && <div className="help is-danger">{this.state.errors.email}</div>}
+                  </div>
+
+                  <div className="register-container">
+                    <div className="field">
+                      <label className="label">Location</label>
+                      <div className="select">
+                        <select name="location" onChange={this.handleChange}>
+                          <option value="">Select</option>
+                          <option value="Amsterdam">Amsterdam</option>
+                          <option value="london">London</option>
+                          <option value="manchester">Manchester</option>
+                          <option value="birmingham">Birmingham</option>
+                          <option value="mexico-city">Mexico City</option>
+                          <option value="Berlin">Berlin</option>
+                          <option value="Paris">Paris</option>
+                          <option value="Brussels">Brussels</option>
+                          <option value="Qubec">Qubec</option>
+                          <option value="Montreal">Montreal</option>
+                          <option value="New York">New York</option>
+                          <option value="Venice">Venice</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div className="field">
+                      <div className="label">D.O.B</div>
+                      <input type="date"
+                        id="dateOfBirth"
+                        name="dateOfBirth"
+                        max="2000-01-01"
+                        onChange={this.handleChange}/>
                     </div>
                   </div>
 
                   <div className="field">
-                    <div className="label">D.O.B</div>
-                    <input type="date" id="dateOfBirth" name="dateOfBirth" max="2000-01-01" onChange={this.handleChange}/>
+                    <label className="label">Password</label>
+                    <div className="control">
+                      <input className="input"
+                        name="password"
+                        type="password"
+                        placeholder="eg: ••••••••"
+                        onChange={this.handleChange} />
+                    </div>
+                    {this.state.errors.password && <div className="help is-danger">{this.state.errors.password}</div>}
                   </div>
-                </div>
 
-                <div className="field">
-                  <label className="label">Password</label>
-                  <div className="control">
-                    <input className="input" name="password" type="password" placeholder="eg: ••••••••" onChange={this.handleChange} />
+                  <div className="field">
+                    <label className="label">Password Confirmation</label>
+                    <div className="control">
+                      <input className="input"
+                        name="passwordConfirmation"
+                        type="password"
+                        placeholder="eg: ••••••••"
+                        onChange={this.handleChange} />
+                    </div>
+                    {this.state.errors.passwordConfirmation && <div className="help is-danger">{this.state.errors.passwordConfirmation}</div>}
                   </div>
-                </div>
-
-                <div className="field">
-                  <label className="label">Password Confirmation</label>
-                  <div className="control">
-                    <input className="input" name="passwordConfirmation" type="password" placeholder="eg: ••••••••" onChange={this.handleChange} />
-                  </div>
-                </div>
-                <br />
-                <button className="button is-info submit-edit-button">Submit</button>
-              </form>
+                  <button className="button is-info submit-edit-button">Submit</button>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
+        </section>
+        <Footer />
       </section>
     )
   }
